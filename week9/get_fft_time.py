e23ac454ca4dc19283ec8c5a588ccee36d33deec
eunbin0-0 <164124235+eunbin0-0@users.noreply.github.com>
2024-03-20 13:09:59 +0900

    Add files via upload

---
+++ week9/get_fft_time.py
@@ -0,0 +1,17 @@
+import numpy as np
+import scipy.io as sio
+import scipy.io.wavfile
+import matplotlib.pyplot as plt
+import argparse 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--filename', required=False, default='filename.wav')
+args = parser.parse_args()
+
+print("drawing plot for", args.filename)
+
+samplerate, data = sio.wavfile.read(args.filename)
+fftsize = len(data)
+data_fft = fftpack.fft(data, fftsize)
+
+# fast fourier transform 
